8f8268e5dff07072872c5984243c0c44bc2c114d
Amlan Saha Kundu <skamlan99@gmail.com>
2019-10-19 09:19:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-#WALLPAPER CHANGER v2.0
+#WALLPAPER CHANGER v2.1
 #By AMLAN SAHA KUNDU
 
 import ctypes, time, random
 
-print(' '+'='*33+'\n'+' Welcome to WALLPAPER CHANGER v2.0 \n\t  by Amlan'+'\n'+' '+'='*33+'\n ')
+print(' '+'='*33+'\n'+' Welcome to WALLPAPER CHANGER v2.1 \n\t  by Amlan'+'\n'+' '+'='*33+'\n ')
 print("Enter r for random choice OR any other key for systematic change")
 uinp = input("Enter your Choice: ")
+val = input("Enter no. of Wallpapers: ")
 num = input("Enter Time Interval(sec): ")
 if uinp == "r":
     while True:
-        path = "C:\\Users\\amlan\\Desktop\\wp\\wp"+str(random.randint(1,10))+".jpg"
+        path = "C:\\Users\\amlan\\Desktop\\wp\\wp"+str(random.randint(1,int(val)))+".jpg"
         print(path)
         ctypes.windll.user32.SystemParametersInfoW(20,0,path,0)
         time.sleep(int(num))
 else:
     n = 1
-    while n < 11:
+    while n < int(val)+1:
         path = "C:\\Users\\amlan\\Desktop\\wp\\wp"+str(n)+".jpg"
         print(path)
         ctypes.windll.user32.SystemParametersInfoW(20,0,path,0)
         time.sleep(int(num))
         n=int(n)+1
-        if n == 11:
+        if n == int(val)+1:
             n = 1
